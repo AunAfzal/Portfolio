73cd537e528085171dc59e281224c86680983241
AunAfzal <144684402+AunAfzal@users.noreply.github.com>
2024-06-28 20:53:10 +0500

    hero fixed

--- app/components/hero/page.js
+++ app/components/hero/page.js
@@ -1,7 +1,9 @@
 export default function Hero() {
     return (
       <section id="hero" className="h-screen flex flex-col justify-center items-center bg-primary text-white text-center p-8">
-        <h1 className="text-6xl font-bold font-poppins mb-4 animate-fadeIn">HEY, I'M <span className="text-accent">MUHAMMAD AUN AFZAL</span></h1>
+        <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold font-poppins mb-4 animate-fadeIn">
+          HEY, I'M <span className="text-accent">MUHAMMAD AUN AFZAL</span>
+        </h1>
         <p className="text-xl mb-8 animate-fadeIn">A skilled web developer, crafting and managing websites and web applications to ensure the success of the entire product with finesse.</p>
         <a href="/Muhammad Aun Afzal_Resume.pdf" download className="bg-accent text-white px-6 py-3 rounded-full font-bold hover:bg-blue-600 transition animate-fadeIn">
           Download CV
